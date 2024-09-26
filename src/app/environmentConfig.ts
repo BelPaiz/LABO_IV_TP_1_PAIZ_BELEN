53d4582feff34dfd46d33b4c3219e3b8781a3fd6
belen <belen.paiz.220686@gmail.com>
2024-09-26 20:25:27 -0300

    sprint 4

--- src/app/environmentConfig.ts
+++
@@ -1,9 +0,0 @@
-export const environment =
-{
-    projectId:"nivel-x--app",
-    appId:"1:45966276192:web:7d2221b4d6b8ddc14e3505",
-    storageBucket:"nivel-x--app.appspot.com",
-    apiKey:"AIzaSyCCZABB1k-EAp41uPaZ09sUkqJXauPAb8o",
-    authDomain:"nivel-x--app.firebaseapp.com",
-    messagingSenderId:"45966276192"
-}
